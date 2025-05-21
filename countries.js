7ebc330740b834e0ff7a2ef0a02ea699a53bf9ad
Payal06012 <payalpatel3727@gmail.com>
2025-05-21 07:55:58 +0530

    adding countries file

---
+++ countries.js
@@ -0,0 +1,12 @@
+const countries={
+  "en-GB": "English",
+  "zh-CN": "Chinese",
+  "hi-IN": "Hindi",
+  "es-ES": "Spanish",
+  "fr-FR": "French",
+  "ar-SA": "Arabic",
+  "bn-IN": "Bengali",
+  "ru-RU": "Russian",
+  "pt-PT": "Portuguese",
+  "ur-PK": "Pakistani"
+}
